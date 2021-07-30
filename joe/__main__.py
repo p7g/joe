6e13debbcdca073e32e4f013d665b5180f70e08c
Patrick Gingras <775.pg.12@gmail.com>
2021-07-29 21:48:59 -0400

    added --dump-ast flag

--- joe/__main__.py
+++ joe/__main__.py
@@ -1,4 +1,4 @@
-def main() -> None:
+def main() -> int:
     import argparse
     import typing as t
     from joe.compile import CompileVisitor
@@ -8,17 +8,33 @@ def main() -> None:
 
     argparser = argparse.ArgumentParser("joe")
     argparser.add_argument("filename")
-    argparser.add_argument("--main", "-m", dest="main_name")
+    argparser.add_argument(
+        "--main",
+        "-m",
+        dest="main_name",
+        help="Specify the path to the main method",
+    )
+    argparser.add_argument(
+        "--dump-ast",
+        dest="dump_ast",
+        action="store_true",
+        help="Dump the AST for the given file and exit",
+    )
 
     args = argparser.parse_args()
     filename: str = args.filename
     main_name: t.Optional[str] = args.main_name
+    dump_ast: bool = args.dump_ast
 
     with open(filename, "r") as f:
         src = f.read()
 
     p = Parser(filename, src)
     mods = p.parse_file()
+
+    if dump_ast:
+        print(mods[0])
+        return 0
 
     ctx = GlobalContext()
     ctx.populate_from_modules(mods)
@@ -33,6 +49,8 @@ def main() -> None:
 
     print(e.get())
 
+    return 0
+
 
 if __name__ == "__main__":
-    main()
+    exit(main())
