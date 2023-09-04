2f31b18b81a0f394e0bbe22d44eb22a7b9cce7d4
Patrick Gingras <775.pg.12@gmail.com>
2023-09-04 09:15:08 -0400

    add data layout string for my machine
    No idea how to get this programmatically yet.

--- joe/__main__.py
+++ joe/__main__.py
@@ -35,6 +35,9 @@ user_main = module.environment.get_type_constructor(main_class).get_method(
 )
 
 ctx = CompileContext()
+ctx.ir_module.data_layout = (
+    "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128"
+)
 compiler = MethodCompiler(ctx, user_main)
 user_main.decl_ast.accept(compiler)
 
